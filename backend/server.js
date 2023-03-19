22c9ec9c8bb78d23c19a43067358a8da4817e9e1
Jorge Alvarado <jalvaradoas39@outlook.com>
2023-03-19 16:55:56 -0400

    Setup backend router

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const router = require('./routes/router');
 
+// setup static files (css, js)
 app.use(express.static('../frontend/public'));
 // setup template engine
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // api routes
-app.get('/', (req, res) => {
-	//res.send('your request to endpoint / returned this response');
-	res.render('home-guest');
-});
+app.use('/', router);
 
 // setup server connection to listen for incoming requests
 const port = process.env.PORT || 5001;
