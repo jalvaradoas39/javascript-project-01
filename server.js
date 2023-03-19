039828a2e29fa5113991c5062bec0aaa6787878e
Jorge Alvarado <jalvaradoas39@outlook.com>
2023-03-19 15:32:13 -0400

    Initial setup, including template engine

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+app.use(express.static('public'));
+// setup template engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// api routes
 app.get('/', (req, res) => {
-	res.send('your request to endpoint / returned this response');
+	//res.send('your request to endpoint / returned this response');
+	res.render('home-guest');
 });
 
+// setup server connection to listen for incoming requests
 const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
